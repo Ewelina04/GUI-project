9dca28f686a85afbd4a5076f370b7b3b5f6d8ca6
Ewelina04 <50328193+Ewelina04@users.noreply.github.com>
2019-06-07 20:44:41 +0200

    dodałam komentarze
    jak coś niejasne to do issues, albo piszcie na grupie

--- GUI game.py
+++ GUI game.py
@@ -1,6 +1,11 @@
 from tkinter import *
 from tkinter import messagebox
+from PIL import Image, ImageTk
+from pygame import mixer
+import random
+import time
 
+# 3 koła ratunkowe w pasku menu, po ich kliknięciu wyświetla się info że one coś tam robią
 def tele():
     messagebox.showinfo("Dzwonimy do Twojego przyjaciela", "H: Witaj! Z tej strony Hubert Urbański z Milionerów.\nTwój przyjaciel gra właśnie o milion i potrzebuje Twojej pomocy przy pytaniu.\nMasz do dyspozycji 4 odpowiedzi.\nP: Myślę, że poprawna jest odpowiedź'...\ni jestem tego pewny na... Mogę się jednak mylić...\n")
 def pol_na_pol():
@@ -8,6 +13,7 @@ def pol_na_pol():
 def publicznosc():
     messagebox.showinfo("pomoc publicznosci", "Proszę publiczność o zagłosowanie na poprawną państwa zdaniem odpowiedź.\nOto wyniki procentowe kolejno dla odp A, B, C i D:")
 
+# prowadzący po kliknięciu wyżwietla info o hubercie, takie jego cv i autopromocja ;-)
 def akcjaAutor():
     messagebox.showinfo("Hubert", "jestem hubert urbański i prowadzę grę milionerzy w tvn\nod pon. do pt. o 20.55\nZapraszam!")
 
@@ -17,6 +23,14 @@ def akcja_przycisk ():
 
 glowne_okno=Tk()
 
+# dźwięk
+file = 'milionmusic.mp3'
+mixer.init()
+mixer.music.load(file)
+mixer.music.play()
+
+
+# tworzone zdjęcie huberta
 canvas_width = 330
 canvas_height =300
 plotno = Canvas(glowne_okno,
@@ -30,10 +44,11 @@ plotno.create_image(200,200,image=imgTk)
 
 pasekMenu = Menu(glowne_okno)
 peirwszeMenu = Menu(pasekMenu, tearoff=0)
+# nazwy okienek u góry(tytuły) po wcisnieciu poszczególnych kół z menu
 peirwszeMenu.add_command(label="Telefon do przyjaciela", command=tele)
 peirwszeMenu.add_command(label="pół na pół", command=pol_na_pol)
 peirwszeMenu.add_command(label="publicznosc", command=publicznosc)
-
+# dalsze menu po lewej u góry co się wyświetla
 peirwszeMenu.add_command(label="wyjdz", command=glowne_okno.quit)
 pasekMenu.add_cascade(label="koła ratunkowe", menu=peirwszeMenu)
 pomocMenu = Menu(pasekMenu, tearoff=0)
@@ -43,8 +58,11 @@ glowne_okno.config(menu=pasekMenu)
 
 glowne_okno.title("OKNO GRY")
 glowne_okno.geometry("700x550")
+
+# to te przyciski odpowiedzi A B C D i ten tekst nad nimi, 
+# ladne kolorki, nie? mogą zostać
 v = IntVar()
-v.set(0)  
+v.set(0)  # to zazanaczona wartosc początkowa czyli 0 = odpowiedzi A
 languages = [
     ("A"),
     ("B"),
@@ -57,6 +75,7 @@ Label(glowne_okno,
          text="""Poprawna odpowiedź na to pytanie to... :""",
          font=("Times New Roman",11,"italic"),
          padx = 20).pack(side=TOP)
+# wygląd przycisków A B C D i ich umiejscowienie
 for val, language in enumerate(languages):
     Radiobutton(glowne_okno,
                   text=language,
